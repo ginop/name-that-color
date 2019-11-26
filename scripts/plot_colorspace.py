38815c6e717965df23e98d2bcfff3011e7c330b1
Gino Perrotta <gperrotta@gwu.edu>
2019-11-25 21:00:27 -0800

    Plot colors to show labels also

--- scripts/plot_colorspace.py
+++ scripts/plot_colorspace.py
@@ -1,10 +1,13 @@
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
 from name_that_color.name_that_color import ntc
 
 
 def main():
-    colors = ntc().names  # initialize class to produce color data-frame from text lists
+    ntc_object = ntc()
+    colors = ntc_object.names  # initialize class to produce color data-frame from text lists
+    colors['category_code'] = colors['basic_name'].apply(ntc_object.shadergb)
 
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
@@ -17,6 +20,22 @@ def main():
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
     ax.scatter3D(colors['hsl_x'], colors['hsl_y'], colors['l'], c=colors['hex_code'])
+    ax.set_xlabel('hsl_x')
+    ax.set_ylabel('hsl_y')
+    ax.set_zlabel('l')
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    ax.scatter3D(colors['r'], colors['g'], colors['b'], c=colors['category_code'])
+    ax.set_xlabel('r')
+    ax.set_ylabel('g')
+    ax.set_zlabel('b')
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    ax.scatter3D(colors['hsl_x'], colors['hsl_y'], colors['l'], c=colors['category_code'])
     ax.set_xlabel('hsl_x')
     ax.set_ylabel('hsl_y')
     ax.set_zlabel('l')
