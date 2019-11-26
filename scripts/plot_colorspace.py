a46897611e01a70bbd249ff4f76fbaef611b06a8
Gino Perrotta <gperrotta@gwu.edu>
2019-11-26 10:05:33 -0800

    Plot all colorspace axes in one figure

--- scripts/plot_colorspace.py
+++ scripts/plot_colorspace.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -6,39 +7,38 @@ from name_that_color.name_that_color import ntc
 
 def main():
     ntc_object = ntc()
-    colors = ntc_object.names  # initialize class to produce color data-frame from text lists
+
+    # colors = ntc_object.names  # initialize class to produce color data-frame from text lists
+    colors = pd.read_pickle('../colors_categorized.pkl').sample(5000)
+
     colors['category_code'] = colors['basic_name'].apply(ntc_object.shadergb)
 
     fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
-    ax.scatter3D(colors['r'], colors['g'], colors['b'], c=colors['hex_code'])
-    ax.set_xlabel('r')
-    ax.set_ylabel('g')
-    ax.set_zlabel('b')
-    plt.show()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
-    ax.scatter3D(colors['hsl_x'], colors['hsl_y'], colors['l'], c=colors['hex_code'])
-    ax.set_xlabel('hsl_x')
-    ax.set_ylabel('hsl_y')
-    ax.set_zlabel('l')
-    plt.show()
+    ax1 = fig.add_subplot(2, 2, 1, projection="3d")
+    ax1.scatter3D(colors['r'], colors['g'], colors['b'], c=colors['hex_code'])
+    ax1.set_xlabel('r')
+    ax1.set_ylabel('g')
+    ax1.set_zlabel('b')
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
-    ax.scatter3D(colors['r'], colors['g'], colors['b'], c=colors['category_code'])
-    ax.set_xlabel('r')
-    ax.set_ylabel('g')
-    ax.set_zlabel('b')
-    plt.show()
+    ax2 = fig.add_subplot(2, 2, 2, projection="3d")
+    ax2.scatter3D(colors['hsl_x'], colors['hsl_y'], colors['l'], c=colors['hex_code'])
+    ax2.set_xlabel('hsl_x')
+    ax2.set_ylabel('hsl_y')
+    ax2.set_zlabel('l')
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
-    ax.scatter3D(colors['hsl_x'], colors['hsl_y'], colors['l'], c=colors['category_code'])
-    ax.set_xlabel('hsl_x')
-    ax.set_ylabel('hsl_y')
-    ax.set_zlabel('l')
+    ax3 = fig.add_subplot(2, 2, 3, projection="3d")
+    ax3.scatter3D(colors['r'], colors['g'], colors['b'], c=colors['category_code'])
+    ax3.set_xlabel('r')
+    ax3.set_ylabel('g')
+    ax3.set_zlabel('b')
+
+    ax4 = fig.add_subplot(2, 2, 4, projection="3d")
+    ax4.scatter3D(colors['hsl_x'], colors['hsl_y'], colors['l'], c=colors['category_code'])
+    ax4.set_xlabel('hsl_x')
+    ax4.set_ylabel('hsl_y')
+    ax4.set_zlabel('l')
+
     plt.show()
 
 
